fe8a64d99dc206df5f878e287f40b0b25a5bae86
Luke <jlukecarlson@gmail.com>
2013-01-26 20:05:26 -0500

    deleted some files

--- Photosphere_Web_Viewer/static/js/photosphereplugin.js
+++ Photosphere_Web_Viewer/static/js/photosphereplugin.js
@@ -2,7 +2,6 @@
   $.fn.makePhotosphere = function() {
     //this.css({"backgroundColor": "black", "color": "white"});
     this.each(function() {
-  		console.log(this);
   		sphere = new Photosphere(this.getAttribute("href"));
 		sphere.loadPhotosphere(this);
 	});
