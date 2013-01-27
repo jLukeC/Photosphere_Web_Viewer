7f55ee3a8ee42fc2523349cb03c4ff8eb49ab268
Luke <jlukecarlson@gmail.com>
2013-01-27 00:39:44 -0500

    adding error handling

--- Photosphere_Web_Viewer/static/js/photosphereplugin.js
+++ Photosphere_Web_Viewer/static/js/photosphereplugin.js
@@ -2,11 +2,18 @@
   $.fn.makePhotosphere = function() {
     //this.css({"backgroundColor": "black", "color": "white"});
     this.each(function() {
+    	try {
   		sphere = new Photosphere(this.getAttribute("href"));
 		sphere.loadPhotosphere(this);
+		}
+		catch (e) {
+			this.append("<img src='" + this.getAttribute("href") + " alt='Photosphere Image'>");
+		}
+
 	});
   };
 })( jQuery );
 $(function() {
-	$('.photosphere').makePhotosphere();
+	//$('.photosphere').makePhotosphere();
+	$('.photosphere').append("<img src='" + this.getAttribute("href") + " alt='Photosphere Image'>");
 });
